de7690c9d47d9edb078ed72bbb73e35226bc29fe
RivanFadlani <rivanfadlani7@gmail.com>
2026-07-03 13:41:51 +0700

    feat: add terminal effect, nvchad treesitter, etc

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -1,1 +1,8 @@
 require "nvchad.autocmds"
+
+vim.schedule(function()
+  local luasnip = require "luasnip"
+  luasnip.filetype_extend("html", { "javascript" })
+  luasnip.filetype_extend("javascriptreact", { "html" })
+  luasnip.filetype_extend("typescriptreact", { "html" })
+end)
